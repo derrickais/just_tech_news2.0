0dfff12622c8fa7308a96663fee41872f24c9653
Derricka S <derrickas728@gmail.com>
2022-07-11 20:16:26 -0400

    ad express sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('sequelize');
 const { Post, User, Comment } = require('../models')
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -32,12 +33,19 @@ router.get('/', (req, res) => {
         }); 
 
         res.render('homepage', { posts });
-        console.log(posts)
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
   
 module.exports = router;
