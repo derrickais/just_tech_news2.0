1f46a62174e3f2b4db26fab4ed32d714bce7c80a
Derricka S <derrickas728@gmail.com>
2022-07-18 14:15:37 -0400

    add and test utils

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({})
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers })
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
